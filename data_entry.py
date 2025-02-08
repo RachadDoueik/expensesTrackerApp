b3547d52a9428fff514650b4d2a620f388169d4a
Rashad Dweik <rashaddweik20@gmail.com>
2025-02-08 14:21:02 +0200

    Done with getters

--- data_entry.py
+++ data_entry.py
@@ -3,6 +3,9 @@ from datetime import datetime
 
 date_format = "%d-%m-%Y"
 
+CATEGORIES = {'I': 'Income' , 'E': 'Expense'}
+
+
 def get_date(prompt , read_default_date=False):
     str_date = input(prompt)
 
@@ -18,10 +21,23 @@ def get_date(prompt , read_default_date=False):
 
 
 def get_amount():
-    pass
+    try:
+        amount = float(input("Enter amount: "))
+        if amount <= 0:
+            raise ValueError("Amount must be non-negative non-zero value !")
+        return amount
+    except ValueError as ve:
+        print(ve)
+        return get_amount()
+
 
 def get_category():
-    pass
+    category = input("Enter category , 'I' for Income and 'E' for Expense: ")
+    if category in CATEGORIES:
+        return CATEGORIES[category]
+    print("Please enter a valid category ! ('I' or 'E')")
+    return get_category()
+    
 
 def get_description():
-    pass
+    description = input("Enter description (optional) : ")
